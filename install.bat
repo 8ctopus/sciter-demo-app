441b566910ed6614a599de202fd969f98da94cb7
8ctopus <hello@octopuslabs.io>
2021-08-27 09:58:16 +0500

    added: pack folder exe

--- install.bat
+++ install.bat
@@ -3,6 +3,9 @@ curl -LO https://github.com/c-smile/sciter-js-sdk/raw/49394612203563e3618a58429f
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/49394612203563e3618a58429f3e50e742f2f3ce/bin/windows/x32/inspector.exe
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/49394612203563e3618a58429f3e50e742f2f3ce/bin/windows/x32/sciter.dll
 
+REM sciter packfolder
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/49394612203563e3618a58429f3e50e742f2f3ce/bin/windows/packfolder.exe
+
 REM sciter package manager 0.1.6
 curl -LO https://github.com/8ctopus/sciter-package-manager/releases/download/0.1.6/spm.phar
 
