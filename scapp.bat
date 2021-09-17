10730944e9a65a2e785c1de4750869f02762ae97
8ctopus <hello@octopuslabs.io>
2021-09-17 09:59:38 +0500

    fixed: scapp bat entry point

--- scapp.bat
+++ scapp.bat
@@ -1,7 +1,7 @@
 tasklist /fi "ImageName eq inspector.exe" /fo csv 2>NUL | find /I "inspector.exe">NUL
 if "%ERRORLEVEL%"=="0" taskkill /IM inspector.exe /f
 
-start bin/inspector.exe
+start bin\inspector.exe
 
 REM wait for inspector to initialize
 sleep 1
@@ -9,4 +9,4 @@ sleep 1
 tasklist /fi "ImageName eq scapp.exe" /fo csv 2>NUL | find /I "scapp.exe">NUL
 if "%ERRORLEVEL%"=="0" taskkill /IM scapp.exe /f
 
-start bin\scapp.exe src\scapp.html --debug
+start bin\scapp.exe src\main.htm --debug
