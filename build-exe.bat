8fe33a0548b2f580bca00fdb41f6590ac78285d8
8ctopus <hello@octopuslabs.io>
2021-10-05 15:39:44 +0500

    improved: build exe using javascript instead of bat script

--- build-exe.bat
+++ build-exe.bat
@@ -1,10 +1,11 @@
 REM pack app resources
-bin\packfolder.exe src bin\demo.dat -binary
+REM bin\packfolder.exe src bin\demo.dat -binary
 
 REM create app icon
-magick -density 256x256 -background transparent src\icon.svg -define icon:auto-resize -colors 256 bin\demo.ico
+REM magick -density 256x256 -background transparent src\icon.svg -define icon:auto-resize -colors 256 bin\demo.ico
 
 REM build app
-bin\x32\scapp.exe build-exe.htm
+start bin\x32\scapp.exe build-exe.htm
 
-pause
+REM pause
+REM magick -define icon:auto-resize src\icon.svg bin\demo.ico
