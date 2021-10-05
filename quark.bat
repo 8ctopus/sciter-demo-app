ead2094a9f5991c9fd1ecadb6f8f6d08aa2c1f15
8ctopus <hello@octopuslabs.io>
2021-10-05 14:42:00 +0500

    added: inspector on quark start

--- quark.bat
+++ quark.bat
@@ -1,1 +1,9 @@
+tasklist /fi "ImageName eq inspector.exe" /fo csv 2>NUL | find /I "inspector.exe">NUL
+if "%ERRORLEVEL%"=="0" taskkill /IM inspector.exe /f
+
+start bin\x32\inspector.exe
+
+REM wait for inspector to initialize
+sleep 1
+
 start bin\x32\scapp.exe quark\index.htm --debug
