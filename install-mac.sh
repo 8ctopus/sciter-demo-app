68436ea4965773daf2666079a2c58d712e3e84f5
8ctopus <hello@octopuslabs.io>
2021-10-11 14:53:18 +0500

    added: mac installation (wip)

---
+++ install-mac.sh
@@ -0,0 +1,22 @@
+mkdir bin/macosx
+
+cd bin/macosx
+
+REM sciter 4.4.8.13
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/macosx/scapp
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/macosx/inspector.app
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/macosx/libsciter.dylib
+
+chmox +x scapp inspector.app libsciter.dylib
+
+REM sciter packfolder
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/macosx/packfolder
+
+chmox +x packfolder
+
+cd ..
+
+REM sciter package manager 0.1.7
+curl -LO https://github.com/8ctopus/sciter-package-manager/releases/download/0.1.7/spm.phar
+
+pause
