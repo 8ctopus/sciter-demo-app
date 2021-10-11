d1ba7aa77142c407bc5a18fdf30bd2b096b74cf2
8ctopus <hello@octopuslabs.io>
2021-10-11 17:13:09 +0500

    fixed: mac installation unzip of sciter binaries

--- install.sh
+++ install.sh
@@ -1,21 +1,18 @@
 mkdir -p bin/macosx
 
-# sciter 4.4.8.13
+cd bin/macosx
+
+# download sciter 4.4.8.13
 curl -LO https://github.com/c-smile/sciter-js-sdk/archive/f810da6a1887220e5d7e9d5a9ec6176508967f0d.zip
 
-unzip f810da6a1887220e5d7e9d5a9ec6176508967f0d.zip bin/macosx/* -d .
+# unzip mac binaries
+unzip f810da6a1887220e5d7e9d5a9ec6176508967f0d.zip sciter-js-sdk-f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/macosx/* -d .
+
+# delete zip
 rm f810da6a1887220e5d7e9d5a9ec6176508967f0d.zip
 
-#curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/macosx/scapp
-#curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/macosx/inspector.app
-#curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/macosx/libsciter.dylib
-
+# make binaries executable
 chmod +x scapp inspector.app libsciter.dylib packfolder
-
-# sciter packfolder
-#curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/macosx/packfolder
-
-#chmod +x packfolder
 
 cd ..
 
@@ -30,4 +27,6 @@ curl -LO https://github.com/8ctopus/sciter-demo-app/releases/download/1.0.1/vend
 
 # unzip dependencies to vendor dir
 unzip vendor.zip
+
+# delete zip
 rm vendor.zip
