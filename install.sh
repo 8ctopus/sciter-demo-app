530cd929f221100044f76e724babb30bc5c8df1f
8ctopus <hello@octopuslabs.io>
2021-10-11 17:20:57 +0500

    fixed: mac installation

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
-mkdir -p bin/macosx
-
-cd bin/macosx
-
 # download sciter 4.4.8.13
 curl -LO https://github.com/c-smile/sciter-js-sdk/archive/f810da6a1887220e5d7e9d5a9ec6176508967f0d.zip
 
-# unzip mac binaries
-unzip -j f810da6a1887220e5d7e9d5a9ec6176508967f0d.zip sciter-js-sdk-f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/macosx/* -d .
+# unzip binaries
+unzip f810da6a1887220e5d7e9d5a9ec6176508967f0d.zip sciter-js-sdk-f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/macosx/* -d .
+
+# move binaries
+mv sciter-js-sdk-f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/macosx bin/macosx
+
+# delete old dir
+rmdir sciter-js-sdk-f810da6a1887220e5d7e9d5a9ec6176508967f0d
 
 # delete zip
 rm f810da6a1887220e5d7e9d5a9ec6176508967f0d.zip
+
+cd bin/macosx
 
 # make binaries executable
 chmod +x scapp inspector.app libsciter.dylib packfolder
