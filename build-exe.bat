70cc434ea39ed0d31922a59a8ce0c863a72d3933
8ctopus <hello@octopuslabs.io>
2021-10-11 12:18:48 +0500

    updated: directory architecture for mac version

--- build-exe.bat
+++ build-exe.bat
@@ -1,12 +1,6 @@
 mkdir bin\tmp
 
-REM pack app resources
-REM bin\packfolder.exe src bin\demo.dat -binary
-
-REM create app icon
-REM magick -density 256x256 -background transparent src\icon.svg -define icon:auto-resize -colors 256 bin\demo.ico
-
 REM build app
-start bin\x32\scapp.exe build-exe.htm
+start bin\win-x32\scapp.exe build-exe.htm
 
 REM pause
--- install.bat
+++ install.bat
@@ -1,21 +1,21 @@
-mkdir bin\x32
-mkdir bin\x64
+mkdir bin\win-x32
+mkdir bin\win-x64
 
-cd bin\x32
+cd bin\win-x32
 
 REM sciter 4.4.8.13
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/windows/x32/scapp.exe
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/windows/x32/inspector.exe
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/windows/x32/sciter.dll
 
-cd ..\x64
+REM sciter packfolder
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/windows/packfolder.exe
+
+cd ..\win-x64
 
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/windows/x64/scapp.exe
 
 cd ..
-
-REM sciter packfolder
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/windows/packfolder.exe
 
 REM sciter package manager 0.1.7
 curl -LO https://github.com/8ctopus/sciter-package-manager/releases/download/0.1.7/spm.phar
