961ef182537b657f1939126d3c5f9a52a28621bf
8ctopus <hello@octopuslabs.io>
2021-11-10 10:16:08 +0500

    cleanup: scapp script

--- scapp.sh
+++ scapp.sh
@@ -8,7 +8,7 @@ case "${uname}" in
 
     *)
         machine="UNKNOWN:${uname}"
-        echo "Cannot install for ${machine}"
+        echo "Cannot run for ${machine}"
         exit
         ;;
 
@@ -17,23 +17,21 @@ esac
 #echo "Detected OS ${machine}"
 
 case "${machine}" in
+    "windows")
+        start bin/win-x32/inspector.exe
+        start bin/win-x32/scapp.exe src/main.htm --debug
+        ;;
 
-"windows")
-    start bin/win-x32/inspector.exe
-    start bin/win-x32/scapp.exe src/main.htm --debug
-    ;;
+    "linux")
+        bin/linux/inspector &
+        bin/linux/scapp src/main.htm --debug &
+        ;;
 
-"linux")
-    bin/linux/inspector &
-    bin/linux/scapp src/main.htm --debug &
-    ;;
+    "macosx")
+        # open inspector
+        open -a inspector.app
 
-"macosx")
-# open inspector
-open -a inspector.app
-
-# open scapp application in debug mode
-    bin/macosx/scapp src/main.htm --debug &
-    ;;
-
+        # open scapp application in debug mode
+        bin/macosx/scapp src/main.htm --debug &
+        ;;
 esac
